40c1f7985d8d50dc11481efba59f051f5ec33161
Song Byungin <byunginhb@gmail.com>
2025-07-07 00:29:42 +0900

    발신번호 입력 필드를 수정하여 기본값을 "123456"으로 설정하고, 입력 타입을 숫자로 변경했습니다. 입력 필드를 읽기 전용으로 설정하여 사용자가 값을 수정할 수 없도록 했습니다.

--- app/send/components/SenderInputCard.tsx
+++ app/send/components/SenderInputCard.tsx
@@ -10,8 +10,10 @@ export default function SenderInputCard({ value, onChange }: Props) {
       <input
         className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-orange-400 transition-all text-gray-900 bg-gray-50"
         placeholder="발신번호 입력"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
+        value="123456"
+        type="number"
+        readOnly
+        onChange={() => {}}
       />
     </div>
   );
